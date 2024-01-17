ca4947d9b43c905517a7f4a33b8330f3d8b89ad6
cheerzhang <zhanglenus@gmail.com>
2024-01-17 14:01:05 +0100

    fix typo

--- df_csv_excel/plot_data.py
+++ df_csv_excel/plot_data.py
@@ -5,20 +5,17 @@ import os
 import plotly.express as px
 
 #####################################################
-#           Plot             #
+#                 Plot histgram                    #
 #####################################################
 def plot_histgram(df, column_name, parameter_root = 0, parameter_log = False):
     if parameter_log:
         df[f'log_{column_name}'] = df[column_name].apply(lambda x: np.log(x+0.00001))
-        df[f'root_{column_name}'] = df[f'root_{column_name}'].apply(lambda x: x.real)
         fig = px.histogram(df, x=f'log_{column_name}')
-        fig.show()
     if parameter_root > 0:
         df[f'root_{column_name}'] = df[column_name].apply(lambda x: x**parameter_root)
         df[f'root_{column_name}'] = df[f'root_{column_name}'].apply(lambda x: x.real)
         fig = px.histogram(df, x=f'root_{column_name}')
-        fig.show()
     if parameter_root == 0 and parameter_log == False:
         fig = px.histogram(df, x=column_name)
-        fig.show()
+    fig.show()
     
