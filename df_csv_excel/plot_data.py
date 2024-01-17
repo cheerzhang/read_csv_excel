175a846a11c76791da88411e3d3e105b65e7e668
cheerzhang <zhanglenus@gmail.com>
2024-01-17 14:09:24 +0100

    add check_normal_distribution

--- df_csv_excel/plot_data.py
+++ df_csv_excel/plot_data.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import os
 import plotly.express as px
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+from scipy.stats import shapiro, anderson
+
+
 
 #####################################################
 #                 Plot histgram                    #
@@ -18,4 +23,30 @@ def plot_histgram(df, column_name, parameter_root = 0, parameter_log = False):
     if parameter_root == 0 and parameter_log == False:
         fig = px.histogram(df, x=column_name)
     fig.show()
+
+
+
+def check_normal_distribution(df, column_name):
+    data = df[column_name]
+    plt.figure(figsize=(12, 6))
+    # Histogram
+    plt.subplot(1, 2, 1)
+    plt.hist(data, bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+    plt.title('Histogram')
+    # QQ Plot
+    plt.subplot(1, 2, 2)
+    stats.probplot(data, dist="norm", plot=plt)
+    plt.title('QQ Plot')
+    plt.show()
+    print("Mean:", data.mean())
+    print("Standard Deviation:", data.std())
+    print("Skewness:", data.skew())
+    print("Kurtosis:", data.kurtosis())
+    # Shapiro-Wilk test
+    stat, p_value = shapiro(data)
+    print(f"Shapiro-Wilk Test: Statistic={stat}, p-value={p_value}")
+    # Anderson-Darling test
+    result = anderson(data)
+    print(f"Anderson-Darling Test: Statistic={result.statistic}, Critical Values={result.critical_values}")
+
     
