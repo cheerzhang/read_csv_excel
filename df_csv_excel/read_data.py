d831bd79543b4f05118d6cee16a0808c9ec20eba
cheerzhang <zhanglenus@gmail.com>
2024-01-22 10:54:13 +0100

    Update read_data.py

--- df_csv_excel/read_data.py
+++ df_csv_excel/read_data.py
@@ -36,6 +36,7 @@ def read_data_by_path(file_path):
 ################################################################
 import json
 
+
 # function to get value of node in json
 def process_json(row, key_names):
     try:
@@ -50,15 +51,18 @@ def process_json(row, key_names):
     except (json.JSONDecodeError, TypeError, KeyError):
         return None
 
+
+
 def get_feature_from_json(df, json_column_name, key_names):
     df['json_feature'] = df[json_column_name].apply(process_json, args=(key_names,))
     return df['json_feature'].values
 
 
+
 def foramt_date_column(df, date_column_name, format=None):
     # format='%Y-%m-%d %H:%M:%S'
     if format is None:
-        df['birthdate'] = pd.to_datetime(df['birthdate'], errors='coerce')
+        df['date_column'] = pd.to_datetime(df[date_column_name], errors='coerce')
     else:
         try:
             df['date_column'] = pd.to_datetime(df[date_column_name], errors='coerce', format=format)
@@ -66,6 +70,7 @@ def foramt_date_column(df, date_column_name, format=None):
         except Exception as e:
             print(e)
             return None
+
 
 
 def format_numeric_column(df, numeric_column_name):
@@ -76,6 +81,7 @@ def format_numeric_column(df, numeric_column_name):
         print(e)
         return None
 
-          
+
+
 def greet(name):
     return f"Hello, {name}!"
